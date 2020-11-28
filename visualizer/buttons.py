98461580cf4059127247b8a23c014f0233099fa5
Cole <cebCole@gmail.com>
2020-11-28 06:41:48 -0600

    more button update.

--- visualizer/buttons.py
+++ visualizer/buttons.py
@@ -1,7 +1,6 @@
 '''
 buttons.py - Button class that has numberous variables to make buttons easier
 '''
-from pyVariables import * #To handle colors
 import pygame
 from pygame.locals import *
 
@@ -21,21 +20,18 @@ class Button:
         color2: Tuple formatted -> color2=(XXX,YYY,ZZZ) - RGB values for color
             of button while button is hovered over, default (255,51,51) - red.
         font: Str, font type for the text, default 'arialblack'
+        font_color: Tuple formatted -> font_color=(XXX,YYY,ZZZ) - RGB values for
+            color of the text, default (255,255,255) - white.
         text: Str, String value of the text to appear on the button.
-        text_color: Tuple formatted -> text_color=(XXX,YYY,ZZZ) - RGB values for
-            color of the text, default (0,0,0) - black.
         display: pygame.Surface object.
         function: Function, the function the button needs to perform on click.
             Passed as so, Button(function=button_function). -> This created a
             button (using the defaults) and passes the function, button_function
             to the button to be called upon click.
-
-
-
     """
     def __init__(self, x=0, y=0, width=50, height=50, font_size=25,
-        color=(255,51,51), color2=(0,255,128), font='arialblack', text='Text',
-        text_color=(0,0,0), display=None, function=None):
+        color=(255,51,51), color2=(0,255,128), font='arialblack',
+        font_color=(255,255,255),  text='Text', display=None, function=None):
 
         self.x = x
         self.y = y
@@ -45,8 +41,8 @@ class Button:
         self.color = color
         self.color2 = color2
         self.font = font
+        self.font_color = font_color
         self.text = text
-        self.text_color = text_color
         self.display = display
         self.function = function
 
@@ -55,7 +51,10 @@ class Button:
         self.click = pygame.mouse.get_pressed()
         self.clicked = False
 
-    def text_length(self):
+
+    def _handle_text_length(self):
+        """For internal use. Handles the sizing of the font so that the text
+         can fit insize of the button."""
         if len(self.text) >= 12:
             self.font_size = self.height/4
         elif len(self.text) >=9:
@@ -63,22 +62,23 @@ class Button:
         elif len(self.text) >= 6:
             self.font_size = self.height/2 - (self.height*0.1)
 
-    def handle_button_colors(self):
-        # if (self.x + self.width > self.mouse[0] > self.x and
-        #     self.y + self.height > self.mouse[1] > self.y):
-
+    def _handle_button_colors(self):
+        """For internal use. Handles the color changing of the button during
+        mouse hover event."""
         if self.button_rect.collidepoint((self.mouse[0], self.mouse[1])):
             pygame.draw.rect(self.display, self.color2,
                             (self.x, self.y, self.width, self.height))
 
-            self.handle_button_click()
+            self._handle_button_click()
 
         else:
             pygame.draw.rect(self.display, self.color,
                             (self.x, self.y, self.width, self.height))
 
 
-    def handle_button_click(self):
+    def _handle_button_click(self):
+        """For internal use. Handles how the button reacts, ie calls function,
+        on a mouse click event, if the function exists."""
         if self.click[0] == 1 and self.function != None:
             if self.click[0] == 0:
                 print(self.click)
@@ -88,14 +88,17 @@ class Button:
             None
 
 
-    def create_button(self):
-        '''Displays the button with text, and multiple colours on the screen'''
-        self.text_length()
-        self.handle_button_colors()
-
+    def _handle_button_text(self):
         font = pygame.font.SysFont(self.font, int(self.font_size))
-        text = font.render(self.text, True, WHITE)
+        text = font.render(self.text, True, self.font_color)
         text_rect = text.get_rect()
         text_rect.center = (self.x + self.width/2,
                             self.y + self.height/2)
         self.display.blit(text, text_rect)
+
+
+    def create_button(self):
+        '''Displays the button with text, and multiple colours on the screen'''
+        self._handle_text_length()
+        self._handle_button_colors()
+        self._handle_buton_text()
